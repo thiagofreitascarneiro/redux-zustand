7f4966d71b4333758b8a4e752df71dcfaf50898d
antena1thiago <thiago.carneiro@antena1.com.br>
2023-10-11 15:42:14 -0300

    create UI with tailwindcss

--- react-redux-zustand/src/App.tsx
+++ react-redux-zustand/src/App.tsx
@@ -1,16 +1,16 @@
 
 import { Provider as ReduxProvider } from "react-redux";
-import { AddTodo } from "./components/AddTodo";
-import { TodoList } from "./components/TodoList";
+import './styles/global.css'
 import { store } from "./store";
+import { Player } from "./pages/Player";
+
 
 function App() {
 
   return (
     <>
       <ReduxProvider store={store}>
-        <TodoList />
-        <AddTodo />
+        <Player />
       </ReduxProvider>
     </>
   )
