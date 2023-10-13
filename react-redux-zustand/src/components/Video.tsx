ea587fdd936471294e997adb5a3f8311ba7f0545
thiagofreitascarneiro <thiagofreitascarneiro@hotmail.com>
2023-10-13 20:27:04 -0300

    feat: create server

--- react-redux-zustand/src/components/Video.tsx
+++ react-redux-zustand/src/components/Video.tsx
@@ -1,14 +1,27 @@
 import ReactPlayer from 'react-player'
+import { useAppSelector } from "../store";
+import { useDispatch } from "react-redux";
+import { next, useCurrentLesson } from "../store/slices/player";
 
 export function Video() {
+  const dispatch = useDispatch()
+
+    const { currentLesson } = useCurrentLesson()
+
+    function handlePlayNext() {
+      dispatch(next())
+    }
+  
   return (
-    <div className="w-full bg-zinc-950 aspect-video">
+      <div className="w-full bg-zinc-950 aspect-video">
       <ReactPlayer
-        width="100%"
-        height="100%"
-        controls
-        url="https://www.youtube.com/watch?v=Jai8w6K_GnY"
+          width="100%"
+          height="100%"
+          controls
+          playing
+          onEnded={handlePlayNext}
+          url={`https://www.youtube.com/watch?v=${currentLesson.id}`}
       />
-    </div>
+      </div>
   );
 }
