c6504bad7cc0efd0e10d66f553ec9c86a51a3dc9
thiagofreitascarneiro <thiagofreitascarneiro@hotmail.com>
2023-10-15 17:25:59 -0300

    feat:create asyncThunk

--- react-redux-zustand/src/components/Header.tsx
+++ react-redux-zustand/src/components/Header.tsx
@@ -1,18 +1,19 @@
+
+import { useCurrentLesson } from "../store/slices/player";
 import { useAppSelector } from "../store";
 
 export function Header() {
-  const { currentModule, currentLesson } = useAppSelector(state => {
-    const { currentModuleIndex, currentLessonIndex } = state.player
-
-    const currentModule = state.player.course.modules[currentModuleIndex]
-    const currentLesson = currentModule.lessons[currentLessonIndex]
-
-    return { currentModule, currentLesson }
-  })
-    return (
-      <div className="flex flex-col gap-1">
-        <h1 className="text-2xl font-bold">{currentLesson.title}</h1>
-        <span className="text-sm text-zinc-400">Módulo "{currentModule.title}"</span>
-      </div>
-    );
+  const { currentModule, currentLesson } = useCurrentLesson();
+  const isCourseLoading = useAppSelector(state => state.player.isLoading)
+  
+  if (isCourseLoading) {
+    return <h1 className="text-2xl font-bold">Carregando...</h1>
   }
+  
+  return (
+    <div className="flex flex-col gap-1">
+      <h1 className="text-2xl font-bold">{currentLesson?.title}</h1>
+      <span className="text-sm text-zinc-400">Módulo "{currentModule?.title}"</span>
+    </div>
+  );
+}
